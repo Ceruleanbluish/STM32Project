58b2d345a515e896ab39a16831fbd4d5dcb17d94
Sai <39679389+Ceruleanbluish@users.noreply.github.com>
2018-05-28 04:18:25 +0800

    Create led.h

---
+++ led.h
@@ -0,0 +1,18 @@
+/*
+ * led.h
+ *
+ *  Created on: 11 11 2017
+ *      Author: Ceruleanbluish
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx.h"
+
+void LED_Init(void);
+void LED_4_Init(void);
+void LED_COMMUNICATION_ERROR(void);
+
+#endif /* LED_H_ */
